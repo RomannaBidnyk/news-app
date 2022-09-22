d41bb4742e0b33fcc94de96ac7e8e87a05532cca
RomannaBidnyk <romanna.bidnyk@gmail.com>
2022-09-22 13:58:37 +0300

    added constants

---
+++ app/src/main/java/com/example/news_app/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.news_app.util
+
+class Constants{
+    companion object {
+        const val API_KEY = "42b4ab8f2c2348b48a88094ccd4d6933"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
