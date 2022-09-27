437aaa92794840ae01fc5251be47b797c4aa3d74
RomannaBidnyk <romanna.bidnyk@gmail.com>
2022-09-27 20:30:15 +0300

    fix db

--- app/src/main/java/com/example/news_app/db/ArticleDatabase.kt
+++ app/src/main/java/com/example/news_app/db/ArticleDatabase.kt
@@ -9,7 +9,8 @@ import com.example.news_app.models.Article
 
 @Database(
     entities = [Article::class],
-    version = 1
+    version = 1,
+    exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class ArticleDatabase : RoomDatabase() {
--- app/src/main/java/com/example/news_app/db/Converters.kt
+++ app/src/main/java/com/example/news_app/db/Converters.kt
@@ -10,6 +10,7 @@ class Converters {
         return source.name
     }
 
+    @TypeConverter
     fun toSource(name: String): Source {
         return Source(name, name)
     }
