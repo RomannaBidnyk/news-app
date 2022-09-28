fab35fdcc445e396032efb5d027b6a706f0cb48c
RomannaBidnyk <romanna.bidnyk@gmail.com>
2022-09-28 18:27:37 +0300

    added possibility to show articles and search for news

--- app/src/main/java/com/example/news_app/util/Constants.kt
+++ app/src/main/java/com/example/news_app/util/Constants.kt
@@ -1,8 +1,9 @@
 package com.example.news_app.util
 
-class Constants{
+class Constants {
     companion object {
         const val API_KEY = "42b4ab8f2c2348b48a88094ccd4d6933"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
